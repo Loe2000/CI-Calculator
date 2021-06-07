ff9a5e9f06c47740160b103bb157ea5fca142ab1
Loe2000 <loe@oregonstate.edu>
2021-06-06 19:10:42 -0700

    added test for multiplication

--- test_calculator.py
+++ test_calculator.py
@@ -10,3 +10,7 @@ class TestCalculatorApp:
 
     def test_sub(self):
         assert 5 == calculator.subtract(10, 5)
+
+    def test_multiply(self):
+        assert 4 == calculator.mutliply(2,2)
+        
